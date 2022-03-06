3476f9c1ec9a3195e526aff435c13785e8d795b4
Christopher J Allen <clevelandclimber@gmail.com>
2022-03-06 18:20:33 -0500

    changed to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_03_06_032413) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,8 +66,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_06_032413) do
   end
 
   create_table "starrings", force: :cascade do |t|
-    t.integer "movie_id", null: false
-    t.integer "actor_id", null: false
+    t.bigint "movie_id", null: false
+    t.bigint "actor_id", null: false
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
